5b08708e4de803c7bcabd3606b2c4bc18f82d6e2
KSJeong771 <25474621+KSJeong771@users.noreply.github.com>
2021-11-27 17:57:35 +0900

    211127

--- _Tools/AutoPush.py
+++ _Tools/AutoPush.py
@@ -1,3 +1,36 @@
 import subprocess
+import datetime as dt
 
-proc = subprocess.call('C:\\Program Files\Git\\git-cmd.exe')
+git_path = 'C:\\Program Files\\Git\\cmd\\git.exe'
+commit_path = 'D:\\Algorithm'
+branch_name = 'main'
+commit_message = dt.datetime.strftime(dt.datetime.now(), '%y%m%d')
+commands = [
+    [git_path, 'add', '*'],
+    [git_path, 'commit', '-m', commit_message],
+    [git_path, 'push', 'origin', branch_name],
+]
+
+def run_commands():
+    for command in commands:
+        print('command : ', end='')
+        print(command)
+            
+        try:
+            outputMessage = subprocess.run(
+                command,
+                cwd=commit_path,
+                check=True,
+                stdout=subprocess.PIPE).stdout
+
+            print("output : ", end='')
+            print(outputMessage)
+        except Exception as e:
+            print("*ERROR : ", end='')
+            print(e)
+            
+def main():
+    run_commands()
+
+if __name__ == "__main__":
+	main()
